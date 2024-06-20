7080f5042d813ae4d73d5bc03ad2940820289ec8
hexiaoyu <1642567075@qq.com>
2024-06-20 20:28:51 +0800

    feat(celery):celery文件

---
+++ task_template/celery.py
@@ -0,0 +1,26 @@
+
+import os
+import django
+from django.conf import settings
+from celery import Celery
+from celery.signals import after_setup_logger, after_setup_task_logger
+# set the default Django settings module for the 'celery' program.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wan_task.settings')
+django.setup()
+
+app = Celery('wan_task')
+
+# Using a string here means the worker doesn't have to serialize
+# the configuration object to child processes.
+# - namespace='CELERY' means all celery-related configuration keys
+#   should have a `CELERY_` prefix.
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# Load task modules from all registered Django app configs.
+# app.autodiscover_tasks()
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS, force=True)
+
+
+@app.task(bind=True)
+def debug_task(self):
+    print('Request: {0!r}'.format(self.request))
