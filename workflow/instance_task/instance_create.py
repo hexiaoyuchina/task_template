f46fa127cb24d61f6be11aa6055eeaa2d59aacf2
hexiaoyu <1642567075@qq.com>
2024-06-22 12:15:58 +0800

    feat(任务类):任务类的prepare,build,finish初版

---
+++ workflow/instance_task/instance_create.py
@@ -0,0 +1,13 @@
+from workflow.core.register_task import register_workflow
+
+
+@register_workflow("instance_create")
+class InstanceCreate():
+    def prepare(self):
+        pass
+
+    def build(self):
+        pass
+
+    def finish(self):
+        pass
