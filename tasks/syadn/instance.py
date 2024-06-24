f4c9f53739ff7a40ad9280c298c186c39a737f4c
hexiaoyu <1642567075@qq.com>
2024-06-24 18:23:49 +0800

    feat(任务类):定义子任务的消息队列为task

--- tasks/syadn/instance.py
+++ tasks/syadn/instance.py
@@ -2,7 +2,7 @@ import logging
 import celery
 from tasks.base import BaseTask
 
-
+# 定义子任务的消息队列为： task
 @celery.task(bind=True, base=BaseTask, Q="task")
 def instance_create_task(self, *args, **kwargs):
     pass
