98c0e687a64629060c362f53cc5b2ef3f5bd8ff1
hexiaoyu <1642567075@qq.com>
2024-07-03 11:17:09 +0800

    feat(日志):celery自定义日志处理

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
-
+# 本地指定的celery log日志
 celery -A task_template worker -l info -c 25 -Q task,workflow -f logs/celery.log
-
-celery -A task_template worker -l info -c 25 -O fair --without-gossip --without-mingle --without-heartbeat
-
-
 celery beat -A task_template -l info -f logs/beat.log
+
+# 运行，使用celerylog日志
+celery -A task_template worker -l info -c 25 -O fair --without-gossip --without-mingle --without-heartbeat
+celery beat -A task_template -l info
+
+
 
 # 查看所有激活队列的
 celery -A  task_template inspect active_queues
--- task_template/celery.py
+++ task_template/celery.py
@@ -1,9 +1,11 @@
 from __future__ import absolute_import, unicode_literals    # 保证 celery.py不和library冲突
 import os
+import logging
 import django
 from django.conf import settings
 from celery import Celery
 from celery.signals import after_setup_logger, after_setup_task_logger
+from utils.log_helper import MyLoggerHandler
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'task_template.settings')
 django.setup()
@@ -24,3 +26,59 @@ app.autodiscover_tasks(lambda: settings.INSTALLED_APPS, force=True)
 @app.task(bind=True)
 def debug_task(self):
     print('Request: {0!r}'.format(self.request))
+
+
+
+# celery customer logger
+class TaskFormatter(logging.Formatter):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        try:
+            from celery._state import get_current_task
+            self.get_current_task = get_current_task
+        except ImportError:
+            self.get_current_task = lambda: None
+
+    def format(self, record):
+        task = self.get_current_task()
+        if task and task.request:
+            workflow_id = ""
+            if task.request.kwargs and task.request.kwargs.get('workflow_id'):
+                workflow_id = task.request.kwargs.get('workflow_id')
+            elif task.request.args and isinstance(task.request.args[0], dict) and task.request.args[0].get('workflow_id'):
+                workflow_id = task.request.args[0].get('workflow_id')
+            if workflow_id:
+                workflow_id = f"{workflow_id}: "
+            record.__dict__.update(task_id=task.request.id, task_name=task.name, workflow_id=workflow_id)
+        else:
+            record.__dict__.setdefault('task_name', '')
+            record.__dict__.setdefault('task_id', '')
+            record.__dict__.setdefault('workflow_id', '')
+        return super().format(record)
+
+
+def create_celery_logger_handler(logger, propagate):
+    celery_handler = MyLoggerHandler(
+        filename=settings.LOG_PATH('worker.log'),
+        when="D",
+        backup_count=365
+    )
+    celery_formatter = TaskFormatter('%(asctime)s - %(levelname)s - %(task_id)s - %(task_name)s - %(name)s::%(lineno)d'
+                                     ' - %(workflow_id)s%(message)s')
+    celery_handler.setFormatter(celery_formatter)
+
+    logger.addHandler(celery_handler)
+    logger.logLevel = logging.INFO
+    logger.propagate = propagate
+
+
+@after_setup_task_logger.connect
+def after_setup_celery_task_logger(logger, **kwargs):
+    """ This function sets the 'celery.task' logger handler and formatter """
+    create_celery_logger_handler(logger, False)
+
+
+@after_setup_logger.connect
+def after_setup_celery_logger(logger, **kwargs):
+    """ This function sets the 'celery' logger handler and formatter """
+    create_celery_logger_handler(logger, False)
